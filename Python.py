18da1baf5312f0667c23a2651c2240cf72a90c01
Gabriel Martínez Llano <gmll920628@gmail.com>
2019-06-21 21:59:19 -0500

    Cambio de una versión a otra utilizando git checkout y haciendo commit con la correción.

--- Python.py
+++ Python.py
@@ -1,15 +1,11 @@
 print("Bienvenido al código de prueba para estudios de repositorio Git")
 print("Este es el primer cambio que realizaremos en el código")
-x1 = 3
-x2 = 5
-y1 = 4
-y2 = 5
+x1=3
+x2=5
+y1=4
+y2=5
 m = (x2-x1)/(y2-y1)
-print("El valor de la pendiente de la recta es {a}".format(a=m))
-print ("Imprimiendo nuevos valores")
-print(x1,x2,y1,y2,m)
 b = y1 - m*x1
-print("El valor del intercepto en el eje Y es {}".format(b))
-print("La ecuación de la recta es y = {m}x + {b}".format(m=m,b=b))
-
-print("El próximo paso será probar uno de los puntos con la ecuación de la recta")
+print("El valor de la pendiente de la recta es {a}".format(a=m))
+print("El valor del intercepto del eje y {}".format(b))
+print ("La función de la recta es y = {m}x + {b}".format(m=m,b=b))
